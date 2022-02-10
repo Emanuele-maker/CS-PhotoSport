10acffa5b2c5e3eb58d5936aacd3ab6664e4569b
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-02-10 19:36:06 +0100

    stop it

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { router: paymentRouter } = require("./routes/paymentRoutes.js")
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, "./build")))
+app.use(express.static(path.join(__dirname)))
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json({
