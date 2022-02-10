7f1ae512745b095da834bf40de9eefe072c69564
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-02-10 18:46:55 +0100

    boh24

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
-"use strict";
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const { router: imagesRouter } = require("./routes/imagesRoutes.js");
-const { router: sessionRouter } = require("./routes/sessionRoutes.js");
-const { router: paymentRouter } = require("./routes/paymentRoutes.js");
-const app = express();
-app.use(express.static(path.join(__dirname, "./client/build")));
-const PORT = process.env.PORT || 5000;
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const { router: imagesRouter } = require("./routes/imagesRoutes.js")
+const { router: sessionRouter } = require("./routes/sessionRoutes.js")
+const { router: paymentRouter } = require("./routes/paymentRoutes.js")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, "./client/build")))
+const PORT = process.env.PORT || 5000
+
 app.use(bodyParser.json({
     limit: '50mb'
-}));
+}))
 app.use(bodyParser.urlencoded({
     limit: '50mb',
     parameterLimit: 100000,
     extended: true
-}));
-app.use("/images", imagesRouter);
-app.use(sessionRouter);
-app.use(paymentRouter);
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+}))
+
+app.use("/images", imagesRouter)
+app.use(sessionRouter)
+app.use(paymentRouter)
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
