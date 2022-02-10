e8cec88e79d0a5098a392f0f0211a53df201db7f
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-02-10 20:43:13 +0100

    changing server approach

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require("path")
+
+const { router: imagesRouter } = require("./routes/imagesRoutes.js")
+const { router: sessionRouter } = require("./routes/sessionRoutes.js")
+const { router: paymentRouter } = require("./routes/paymentRoutes.js")
+
+const app = express()
+
+app.use(express.static(path.join(__dirname)))
+
+app.use(bodyParser.json({
+    limit: '50mb'
+}))
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 100000,
+    extended: true
+}))
+
+app.use("/images", imagesRouter)
+app.use(sessionRouter)
+app.use(paymentRouter)
+
+module.exports = app
--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const path = require("path")
+const app = require("./app")
+const http = require("http")
 
-const { router: imagesRouter } = require("./routes/imagesRoutes.js")
-const { router: sessionRouter } = require("./routes/sessionRoutes.js")
-const { router: paymentRouter } = require("./routes/paymentRoutes.js")
-
-const app = express()
-
-app.use(express.static(path.join(__dirname)))
+const server = http.createServer(app)
 const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.json({
-    limit: '50mb'
-}))
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    parameterLimit: 100000,
-    extended: true
-}))
-
-app.use("/images", imagesRouter)
-app.use(sessionRouter)
-app.use(paymentRouter)
-
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+server.listen(PORT)
