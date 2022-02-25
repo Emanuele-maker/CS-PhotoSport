7a3769738481b7b72915917cc336e95b53a524d6
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-02-25 17:53:05 +0100

    testing cd bat

--- build.bat
+++ build.bat
@@ -1,15 +1,16 @@
-call node ./loadImages.js
-call del .\static\js\* /Q
-call del .\static\css\* /Q
-call del .\static\media\* /Q
-call cd client
-call npm run build
-call robocopy .\build ..\ * /mt /move
-cd .\build\static
-call robocopy .\js ..\..\..\static\js * /move /mt
-call robocopy .\css ..\..\..\static\css * /move /mt
-call robocopy .\media ..\..\..\static\media * /move /mt
-cd ..\..
-call rmdir .\build\static /Q
-call del .\build\* /Q
-call rmdir .\build /Q
+@REM call node ./loadImages.js
+@REM call del .\static\js\* /Q
+@REM call del .\static\css\* /Q
+@REM call del .\static\media\* /Q
+call cd client/
+call del .\test.txt
+@REM call npm run build
+@REM call robocopy .\build ..\ * /mt /move
+@REM cd .\build\static
+@REM call robocopy .\js ..\..\..\static\js * /move /mt
+@REM call robocopy .\css ..\..\..\static\css * /move /mt
+@REM call robocopy .\media ..\..\..\static\media * /move /mt
+@REM cd ..\..
+@REM call rmdir .\build\static /Q
+@REM call del .\build\* /Q
+@REM call rmdir .\build /Q
