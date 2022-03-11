fcc91558d0f563da8aae38738f108ec2d29ce644
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-03-11 22:29:38 +0100

    z-index fix

--- client/src/components/Layout/Copyright/CopyRight.jsx
+++ client/src/components/Layout/Copyright/CopyRight.jsx
@@ -2,8 +2,8 @@ import "./CopyRight.scss"
 
 export default function CopyRight() {
     return (
-        <footer>
-            <h1 className="footer-title">© { new Date().getFullYear() } CS PhotoSport Tutti i diritti riservati</h1>
-        </footer>
+        <div className="copyright">
+            <h1 className="copyright-title">© { new Date().getFullYear() } CS PhotoSport Tutti i diritti riservati</h1>
+        </div>
     )
 }
