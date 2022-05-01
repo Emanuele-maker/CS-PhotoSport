6b0000c7572aaa4af61b9dac9697c0f6add57044
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-01 14:55:12 +0200

    custom download event

--- client/src/components/ImageCard/ImageCard.jsx
+++ client/src/components/ImageCard/ImageCard.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react"
 import LazyImage from "../LazyImage"
 import "./ImageCard.scss"
 
 export default function ImageCard({ imageName, imageSrc, download }) {
+  useEffect(() => {
+    const button = document.getElementById(imageName)
+    button.addEventListener("click", () => {
+      download(imageName)
+      window.gtag("event", "download", {
+        event_category: "download",
+        event_label: imageName,
+      })
+    })
+  }, [download, imageName])
+
   return (
     <div className="image-container">
         <LazyImage src={imageSrc} alt="" />
-        <button className="download-btn" onClick={() => download(imageName)}>Scarica</button>
+        <button className="download-btn" id={imageName}>Scarica</button>
     </div>
   )
 }
