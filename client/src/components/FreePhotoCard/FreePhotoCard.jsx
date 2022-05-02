cc114ec467e8c04ed99be7dd59106643d9b1f604
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-02 16:42:23 +0200

    class id download image link

--- client/src/components/FreePhotoCard/FreePhotoCard.jsx
+++ client/src/components/FreePhotoCard/FreePhotoCard.jsx
@@ -1,12 +1,13 @@
 import LazyImage from "../LazyImage"
 import "./FreePhotoCard.scss"
 
-export default function FreePhotoCard({ preview }) {
+export default function FreePhotoCard({ preview, imageName }) {
   function downloadImage(imageSrc) {
     const link = document.createElement('a')
     link.href = imageSrc
     link.download = "CS PhotoSport"
     link.setAttribute("class", "download-link")
+    link.setAttribute("id", imageName)
     document.body.appendChild(link)
     link.click()
     document.body.removeChild(link)
