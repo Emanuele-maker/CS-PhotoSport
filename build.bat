2477f0b02825d29b51bc02f08eaeb880f43fc75d
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-16 16:45:51 +0200

    buildImages command

--- build.bat
+++
@@ -1,5 +0,0 @@
-@REM call break>.\client\src\images.json
-@REM call break>.\client\src\previews.json
-@REM call node ./loadImages.js
-call cd client/
-npm run build
---
+++ buildImages.bat
@@ -0,0 +1,3 @@
+call break>.\client\src\images.json
+call break>.\client\src\previews.json
+call node ./loadImages.js
