fde494c42271a383125a94bfb2d67a39f8e85c97
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-22 15:07:40 +0200

    SiteLogo hot fix

--- client/src/staticInfo.js
+++ client/src/staticInfo.js
@@ -1,7 +1,7 @@
 const siteRoute = process.env.NODE_ENV === "production" ? "https://csphotosport.com" : "http://localhost:5000"
 const previewsRoute = process.env.NODE_ENV === "production" ? `${siteRoute}/client/build/previews` : `${siteRoute}/previews`
 const imagesRoute = process.env.NODE_ENV === "production" ? `${siteRoute}/client/build/img` : `${siteRoute}/img`
-const buildRoute = process.env.NODE_ENV === "production" ? `http://localhost:3000/client/build` : "https://csphotosport.com/client/build"
+const buildRoute = process.env.NODE_ENV === "production" ? "https://csphotosport.com/client/build" : `http://localhost:3000/client/build`
 
 export {
     siteRoute,
