435d5c968a1d340528da66875bc9d78afb8808d4
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2022-05-22 15:09:43 +0200

    About Page hot fix

--- client/src/components/pages/About/About.jsx
+++ client/src/components/pages/About/About.jsx
@@ -29,9 +29,9 @@ const About = () => {
             </h3>
             <AboutContainer animDuration={animDuration} animDelay={animDuration * 2 - 150} imageSrc={`${previewsRoute}/Running/Vivicittà Latina 2022/1 2 6.jpg`} imageAlt="Running" title="RUNNING" href="/running" paragraph="Gare su strada a tutti i livelli" justify="left" bgColor="lightblue" />
             <AboutContainer animDuration={animDuration} animDelay={animDuration * 2 - 150} imageSrc={`${previewsRoute}/Kite, Windsurf & Windfoil/Fogliano 20 Maggio 2022/IMG_4154.jpg`} imageAlt="Kite & WindSurf" title="KITE & WINDSURF" href="/kite,-windsurf-&-windfoil" paragraph="" justify="right" bgColor="#fff993" />
-            <AboutContainer animDuration={animDuration} animDelay={animDuration * 2 - 150} imageSrc={`${previewsRoute}/Pallanuoto/Castelli Romani vs CN Latina/IMG_9386.jpg`} imageAlt="Pallanuoto" title="PALLANUTO" href="/pallanuoto" paragraph="Partite di livello giovanile e professionale" justify="left" bgColor="#ff8e8e" />
-            <AboutContainer animDuration={animDuration} animDelay={animDuration * 2 - 250} imageSrc={`${previewsRoute}/Triathlon/The Green Race Ciclismo/IMG_0723.jpg`} imageAlt="Triathlon" title="TRIATHLON" href="/triathlon" paragraph="Competizioni regionali e nazionali" justify="right" bgColor="#9ffcc9" />
-            <AboutContainer animDuration={animDuration} animDelay={animDuration * 2 - 250} imageSrc={require("../../../cover/Cover FIPAV 27.3.2022.jpg")} imageAlt="Beach Volley" title="BEACH VOLLEY" href="/beachvolley" paragraph="Tornei amatoriali e professionali" justify="left" bgColor="#f7d7a0" />
+            <AboutContainer animDuration={animDuration} animDelay={100} imageSrc={`${previewsRoute}/Pallanuoto/Castelli Romani vs CN Latina/IMG_9386.jpg`} imageAlt="Pallanuoto" title="PALLANUTO" href="/pallanuoto" paragraph="Partite di livello giovanile e professionale" justify="left" bgColor="#ff8e8e" />
+            <AboutContainer animDuration={animDuration} animDelay={100} imageSrc={`${previewsRoute}/Triathlon/The Green Race Ciclismo/IMG_0723.jpg`} imageAlt="Triathlon" title="TRIATHLON" href="/triathlon" paragraph="Competizioni regionali e nazionali" justify="right" bgColor="#9ffcc9" />
+            <AboutContainer animDuration={animDuration} animDelay={100} imageSrc={require("../../../cover/Cover FIPAV 27.3.2022.jpg")} imageAlt="Beach Volley" title="BEACH VOLLEY" href="/beachvolley" paragraph="Tornei amatoriali e professionali" justify="left" bgColor="#f7d7a0" />
         </div>
     )
 }
