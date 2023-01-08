07291ad9befe08f3864055e7031e67b7f66aa857
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2023-01-08 17:29:48 +0100

    Lazio vs Campania

--- client/src/staticInfo.js
+++ client/src/staticInfo.js
@@ -1,4 +1,5 @@
 const siteRoute = process.env.NODE_ENV === "production" ? "https://csphotosport.com" : "http://localhost:5000"
+// const siteRoute = "http://localhost:5000"
 const previewsRoute = process.env.NODE_ENV === "production" ? `${siteRoute}/client/build/previews` : `${siteRoute}/previews`
 const imagesRoute = process.env.NODE_ENV === "production" ? `${siteRoute}/client/build/img` : `${siteRoute}/img`
 const coverRoute = process.env.NODE_ENV === "production" ? `${siteRoute}/client/build/cover` : `${siteRoute}/cover`
