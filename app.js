9be8c18205f8b6ad79570fdb2c4999fe81302e42
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2023-01-13 21:46:07 +0100

    Lazio vs Toscana

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ const baseRoute = "/api"
 app.use(baseRoute, sessionRouter)
 app.use(baseRoute, paymentRouter)
 app.use(baseRoute, userRouter)
-
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build/index.html'))
 })
