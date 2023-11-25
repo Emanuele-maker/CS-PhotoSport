83ae77aa970cf5dfda76822f58dd9ec3c33d6d63
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2023-11-25 14:32:07 +0100

    ultimate fixing p1

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require("./app")
 const http = require("http")
 require("dotenv").config()
-require("./Feedback bot/index")
 
 const server = http.createServer(app)
 const PORT = process.env.PORT || 5000
