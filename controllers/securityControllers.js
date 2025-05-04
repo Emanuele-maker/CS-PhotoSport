f198cfbc143de51826ea598076d760ac12099c07
Manu_Dev <65360943+Emanuele-maker@users.noreply.github.com>
2025-05-04 11:10:26 +0200

    fixing cors #2

--- controllers/securityControllers.js
+++ controllers/securityControllers.js
@@ -1,11 +1,12 @@
 require("dotenv").config()
 
 module.exports = (req, res, next) => {
-    const allowedOrigins = process.env.NODE_ENV === "production" ? ['http://csphotosport.com', "https://beluga-fll.netlify.app"] : ['http://csphotosport.com', 'http://localhost:3000', 'http://localhost:5000', "http://127.0.0.1:5500/", "http://127.0.0.1:5501/"]
-    const origin = req.headers.origin
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin)
-    }
+    // const allowedOrigins = process.env.NODE_ENV === "production" ? ['http://csphotosport.com', "https://beluga-fll.netlify.app"] : ['http://csphotosport.com', 'http://localhost:3000', 'http://localhost:5000', "http://127.0.0.1:5500/", "http://127.0.0.1:5501/"]
+    // const origin = req.headers.origin
+    // if (allowedOrigins.includes(origin)) {
+    //     res.setHeader('Access-Control-Allow-Origin', origin)
+    // }
+    res.setHeader('Access-Control-Allow-Origin', "*")
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.header('Access-Control-Allow-Credentials', true)
